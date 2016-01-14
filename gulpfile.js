e09dd92362918bc970e2e4a71f4901404ba68633
knewhouse <newhousemusic@gmail.com>
2016-01-14 10:25:14 -0800

    added gulpif and minified js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
     concat = require('gulp-concat');
 
 var env,
@@ -24,7 +26,6 @@ if (env==='development') {
   outputDir = 'builds/production/';
   sassStyle = 'compressed';
 }
-
 
 coffeeSources = ['components/coffee/tagline.coffee'];
 jsSources = [
@@ -48,6 +49,7 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
     .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload())
 });
